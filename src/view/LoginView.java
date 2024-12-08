3f794fb1ce59cb1f9812de7d1f9f5df3afede0b9
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-08 16:09:50 +0700

    added showAlert method

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -127,5 +127,12 @@ public class LoginView {
             showAlert("Error", "Database connection error!", Alert.AlertType.ERROR);
         }
     }
+    private void showAlert(String title, String content, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 
 }
