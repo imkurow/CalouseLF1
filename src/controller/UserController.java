990cdd6b0dff1bfd3089067b82381ce7fae074e1
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-08 15:47:39 +0700

    added login method

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import models.User;
+
 public class UserController{
 	private Connection conn;
 	
@@ -71,6 +73,31 @@ public class UserController{
 	            e.printStackTrace();
 	            return false;
 	        }
+	 }
+	 
+	 //LOGIN	
+	 public User login(String username, String password) {
+		 String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+	        try {
+	            PreparedStatement ps = conn.prepareStatement(query);
+	            ps.setString(1, username);
+	            ps.setString(2, password);
+	            
+	            ResultSet rs = ps.executeQuery();
+	            if(rs.next()) {
+	                return new User(
+	                    rs.getString("user_id"),
+	                    rs.getString("username"),
+	                    rs.getString("password"),
+	                    rs.getString("phone_number"),
+	                    rs.getString("address"),
+	                    rs.getString("role")
+	                );
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return null;
 	 }
 	 
 	 
