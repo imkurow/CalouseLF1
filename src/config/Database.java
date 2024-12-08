81493febda507926b39a0c1fca64c48c3d0a3eae
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-08 15:25:20 +0700

    update database connection

--- src/config/Database.java
+++ src/config/Database.java
@@ -4,25 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Database {
-	private static final String URL = "jdbc:mysql://localhost:3306/calouself";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
-    private static Database instance = null;
-    
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    public static Database getInstance() {
-    	if(instance == null) {
-    		instance = new Database();
-    	}
-    	return instance;
-    }
+	 private static Database instance;
+	    private Connection connection;
+	    
+	    private final String USERNAME = "root";
+	    private final String PASSWORD = "";
+	    private final String DATABASE = "calouself";
+	    private final String HOST = "localhost:3306";
+	    private final String CONNECTION = String.format("jdbc:mysql://%s/%s", HOST, DATABASE);
+	    
+	    private Database() {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            connection = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    public static Database getInstance() {
+	        if (instance == null) {
+	            synchronized(Database.class) {
+	                if (instance == null) {
+	                    instance = new Database();
+	                }
+	            }
+	        }
+	        return instance;
+	    }
+	    
+	    public Connection getConnection() {
+	        return connection;
+	    }
 }
