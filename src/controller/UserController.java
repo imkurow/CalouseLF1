ce8885a2d34b63d4de99fd99373315f0d793b16c
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-08 15:36:16 +0700

    create password validation

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -9,6 +9,15 @@ public class UserController{
 	 private boolean validateUsername(String username) {
 	        return username.length() >= 3;
 	 }
+	 private boolean validatePassword(String password) {
+	        if(password.length() < 8) return false;
+	        String specialChars = "!@#$%^&*";
+	        for(char c : password.toCharArray()) {
+	            if(specialChars.indexOf(c) != -1) return true;
+	        }
+	        return false;
+	 }
+
 	
 	
 }
