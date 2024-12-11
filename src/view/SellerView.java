a429e9fc003c751fb34df9225cf26b7ed543e689
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 15:28:26 +0700

    created form layout

--- src/view/SellerView.java
+++ src/view/SellerView.java
@@ -1,7 +1,10 @@
 package view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -37,4 +40,39 @@ public class SellerView {
         stage.show();
     }
 
+    private VBox createHeaderBox() {
+        VBox headerBox = new VBox(10);
+        headerBox.setAlignment(Pos.CENTER);
+        headerBox.setPadding(new Insets(0, 0, 20, 0));
+        
+        Label welcomeLabel = new Label("Welcome, " + seller.getUsername() + "!");
+        welcomeLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        
+        Label roleLabel = new Label("Seller Dashboard");
+        roleLabel.setStyle("-fx-font-size: 18px;");
+        
+        headerBox.getChildren().addAll(welcomeLabel, roleLabel);
+        return headerBox;
+    }
+    
+    private VBox createMenuBox() {
+        VBox menuBox = new VBox(15);
+        menuBox.setAlignment(Pos.CENTER);
+        
+        Button uploadItemBtn = new Button("Upload New Item");
+        Button viewItemsBtn = new Button("View My Items");
+        Button logoutBtn = new Button("Logout");
+        
+        // Set button sizes
+        uploadItemBtn.setPrefWidth(200);
+        viewItemsBtn.setPrefWidth(200);
+        logoutBtn.setPrefWidth(200);
+        
+        // Button actions
+        uploadItemBtn.setOnAction(e -> showUploadItemView());
+        logoutBtn.setOnAction(e -> handleLogout());
+        
+        menuBox.getChildren().addAll(uploadItemBtn, viewItemsBtn, logoutBtn);
+        return menuBox;
+    }
 }
