208f4859c53482456531cf8bb7bb50fe2466bee3
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 11:51:06 +0700

    added generateItemId method

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -1,5 +1,39 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import config.Database;
+
 public class ItemController {
+	private Connection conn;
+	
+	public ItemController() {
+		try {
+            this.conn = Database.getInstance().getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	private String generateItemId() {
+        String query = "SELECT item_id FROM items ORDER BY item_id DESC LIMIT 1";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                String lastId = rs.getString("item_id");
+                int number = Integer.parseInt(lastId.substring(2)) + 1;
+                return String.format("IT%03d", number);
+            }
+            return "IT001"; 
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "IT" + System.currentTimeMillis() % 1000;
+        }
+    }
 	
 }
