a2976092c87b69c2db00e7aef94470f5bf3a21dc
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 12:01:10 +0700

    initialized UploadItemView class

---
+++ src/view/UploadItemView.java
@@ -0,0 +1,18 @@
+package view;
+
+import controller.ItemController;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import models.User;
+
+public class UploadItemView {
+	
+	private Stage stage;
+	private ItemController itemController;
+	private User seller; 
+	private Scene scene;
+	
+	
+	
+	
+}
