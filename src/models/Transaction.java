a871c42c48fdebbc8e9ee3a86ce5add7aa1a281f
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 22:35:57 +0700

    added getter&setter

--- src/models/Transaction.java
+++ src/models/Transaction.java
@@ -10,4 +10,30 @@ public class Transaction {
         this.userId = userId;
         this.itemId = itemId;
     }
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+    
+    
 }
