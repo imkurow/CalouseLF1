ebdd9f8b4fbe42e34f3d71bad91dca9f01dc40b4
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 16:37:10 +0700

    initialized EditItemView class

---
+++ src/view/EditItemView.java
@@ -0,0 +1,17 @@
+package view;
+
+import controller.ItemController;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import models.Item;
+
+public class EditItemView {
+
+	private Stage stage;
+    private Scene scene;
+    private Item item;
+    private ItemController itemController;
+    private Runnable onUpdateCallback;
+    
+	
+}
