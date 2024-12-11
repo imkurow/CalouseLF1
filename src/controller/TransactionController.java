a31156cf92731a6715c5dac44c2ad42321f0f3dc
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 22:39:58 +0700

    added generateTransactionIdd method

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import config.Database;
 
@@ -12,6 +15,24 @@ public class TransactionController {
             this.conn = Database.getInstance().getConnection();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    
+    private String generateTransactionId() {
+        String query = "SELECT transaction_id FROM transactions ORDER BY transaction_id DESC LIMIT 1";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                String lastId = rs.getString("transaction_id");
+                int number = Integer.parseInt(lastId.substring(2)) + 1;
+                return String.format("TR%03d", number);
+            }
+            return "TR001";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "TR" + System.currentTimeMillis() % 1000;
         }
     }
 }
