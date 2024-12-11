bcecb7c60a33a47320dc0b79787edf2246d497fd
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-11 14:42:36 +0700

    added constructor

--- src/view/SellerView.java
+++ src/view/SellerView.java
@@ -1,5 +1,19 @@
 package view;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import models.User;
+
 public class SellerView {
+	 
+    private Stage stage;
+    private Scene scene;
+    private User seller;
+    
+    public SellerView(Stage stage, User seller) {
+        this.stage = stage;
+        this.seller = seller;
+        initializeSellerView();
+    }
 
 }
