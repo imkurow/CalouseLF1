35700e04f36a8a1398797a91e9546d72ff9469e7
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-12 09:02:06 +0700

    initialized Offer class

---
+++ src/models/Offer.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Offer {
+	private String offerId;
+    private String userId;
+    private String itemId;
+    private double offerPrice;
+    private String offerStatus;
+}
