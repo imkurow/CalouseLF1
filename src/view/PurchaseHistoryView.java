43a2e8ff3c5cde3a83f6504e8fe17bc13c05ac29
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-12 20:43:24 +0700

    added page layout for PurchaseHistory

--- src/view/PurchaseHistoryView.java
+++ src/view/PurchaseHistoryView.java
@@ -2,13 +2,16 @@ package view;
 
 import controller.ItemController;
 import controller.TransactionController;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import models.Item;
 import models.Transaction;
 import models.User;
 
@@ -45,6 +48,44 @@ public class PurchaseHistoryView {
         stage.setScene(scene);
         stage.setTitle("Purchase History");
         stage.show();
+    }
+    
+    private VBox createTableView() {
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(10));
+        
+        TableColumn<Transaction, String> transactionIdCol = new TableColumn<>("Transaction ID");
+        transactionIdCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getTransactionId()));
+        
+        TableColumn<Transaction, String> itemNameCol = new TableColumn<>("Item Name");
+        itemNameCol.setCellValueFactory(data -> {
+            Item item = itemController.getItemById(data.getValue().getItemId());
+            return new SimpleStringProperty(item != null ? item.getName() : "Unknown");
+        });
+        
+        TableColumn<Transaction, String> categoryCol = new TableColumn<>("Category");
+        categoryCol.setCellValueFactory(data -> {
+            Item item = itemController.getItemById(data.getValue().getItemId());
+            return new SimpleStringProperty(item != null ? item.getCategory() : "Unknown");
+        });
+        
+        TableColumn<Transaction, String> sizeCol = new TableColumn<>("Size");
+        sizeCol.setCellValueFactory(data -> {
+            Item item = itemController.getItemById(data.getValue().getItemId());
+            return new SimpleStringProperty(item != null ? item.getSize() : "Unknown");
+        });
+        
+        TableColumn<Transaction, String> priceCol = new TableColumn<>("Price");
+        priceCol.setCellValueFactory(data -> {
+            Item item = itemController.getItemById(data.getValue().getItemId());
+            return new SimpleStringProperty(item != null ? String.format("$%.2f", item.getPrice()) : "Unknown");
+        });
+        
+        tableView.getColumns().addAll(transactionIdCol, itemNameCol, categoryCol, sizeCol, priceCol);
+        refreshTableData();
+        
+        vbox.getChildren().add(tableView);
+        return vbox;
     }
 
 }
