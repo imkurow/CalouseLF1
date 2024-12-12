fb9f636d3d99e2b8b3e92c9e122ae3c50112a4d7
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-12 18:34:45 +0700

    added generateWishlistId method

--- src/controller/WishlistController.java
+++ src/controller/WishlistController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import config.Database;
 
@@ -15,6 +18,24 @@ public class WishlistController {
             this.itemController = new ItemController();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    
+    private String generateWishlistId() {
+        String query = "SELECT wishlist_id FROM wishlists ORDER BY wishlist_id DESC LIMIT 1";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                String lastId = rs.getString("wishlist_id");
+                int number = Integer.parseInt(lastId.substring(2)) + 1;
+                return String.format("WL%03d", number);
+            }
+            return "WL001";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "WL" + System.currentTimeMillis() % 1000;
         }
     }
     
